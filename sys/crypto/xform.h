9a9984783f68e0a4248de23d82427541058fa972
Mike Belopuhov <mike@belopuhov.com>
2017-04-09 21:17:08 +0200

    Convert xforms from Rijndael to the new AES

--- sys/crypto/xform.h
+++ sys/crypto/xform.h
@@ -79,7 +79,7 @@ union authctx {
 extern struct enc_xform enc_xform_3des;
 extern struct enc_xform enc_xform_blf;
 extern struct enc_xform enc_xform_cast5;
-extern struct enc_xform enc_xform_rijndael128;
+extern struct enc_xform enc_xform_aes;
 extern struct enc_xform enc_xform_aes_ctr;
 extern struct enc_xform enc_xform_aes_gcm;
 extern struct enc_xform enc_xform_aes_gmac;
